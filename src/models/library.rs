96693f729d8b96a324a0fb336134a0ce0ee2ea40
Jason Sanders <fictitiouswizard@gmail.com>
2024-03-23 18:29:20 -0500

    213

--- src/models/library.rs
+++ src/models/library.rs
@@ -1,21 +1,135 @@
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
+// Conditional compilation for sqlx when either test or ssr feature is enabled
 #[cfg(any(test, feature = "ssr"))]
-use {
-    sqlx
-};
+use sqlx;
+use crate::models::library_system::LibrarySystem;
 
-
+// Derive sqlx::FromRow trait for Library struct when either ssr or test feature is enabled
+// This trait allows Library instances to be created from a database row
 #[cfg_attr(any(feature = "ssr", test), derive(sqlx::FromRow))]
+// Derive Serialize and Deserialize traits for Library struct
+// These traits allow Library instances to be converted to and from JSON
 #[derive(Serialize, Deserialize)]
+// Library struct represents a library entity in the application
 pub struct Library {
+    // Unique identifier for the library
     pub id: Uuid,
+    // Name of the library
     pub name: String,
+    // URL-friendly version of the library name
     pub slug: String,
+    // Street address of the library
     pub street_address: String,
+    // City where the library is located
     pub city: String,
+    // State or province where the library is located
     pub state_or_province: String,
+    // Postal code of the library's location
     pub postal_code: String,
+    // Current point balance of the library
     pub point_balance: u64,
+    // Identifier of the library system to which the library belongs
+    pub library_system_id: Uuid,
+}
+
+// CreateLibrary struct represents the data required to create a new library
+#[derive(Serialize, Deserialize)]
+pub struct CreateLibrary {
+    // Name of the library
+    pub name: String,
+    // URL-friendly version of the library name
+    pub slug: String,
+    // Street address of the library
+    pub street_address: String,
+    // City where the library is located
+    pub city: String,
+    // State or province where the library is located
+    pub state_or_province: String,
+    // Postal code of the library's location
+    pub postal_code: String,
+}
+
+// Conditional compilation for sqlx when either test or ssr feature is enabled
+#[cfg(any(test, feature = "ssr"))]
+// Function to create a new library in the database
+pub async fn create_library(pool: &sqlx::PgPool, library: CreateLibrary, library_system: LibrarySystem) -> Result<Library, sqlx::Error> {
+    // Execute an SQL query to insert a new library into the database
+    let result : Library = sqlx::query_as::<_, Library>(
+        "insert into library (name, slug, street_address, city, state_or_province, postal_code, point_balance, library_system_id) values ($1, $2, $3, $4, $5, $6, 0, $7) returning id, name, slug, street_address, city, state_or_province, postal_code, point_balance, library_system_id",
+    )
+        .bind(library.name)
+        .bind(library.slug)
+        .bind(library.street_address)
+        .bind(library.city)
+        .bind(library.state_or_province)
+        .bind(library.postal_code)
+        .bind(library_system.id)
+        .fetch_one(pool).await?;
+    // Return the newly created library
+    Ok(result)
+}
+
+// Conditional compilation for sqlx when either test or ssr feature is enabled
+#[cfg(any(test, feature = "ssr"))]
+// Function to retrieve a library by its unique identifier from the database
+pub async fn get_library_by_id(pool: &sqlx::PgPool, id: Uuid) -> Result<Library, sqlx::Error> {
+    // Execute an SQL query to select a library by its unique identifier
+    let result = sqlx::query_as::<_, Library>(
+        "select * from library where id = $1"
+    ).bind(id).fetch_one(pool).await?;
+    // Return the library if found
+    Ok(result)
+}
+
+// Conditional compilation for sqlx when either test or ssr feature is enabled
+#[cfg(any(test, feature = "ssr"))]
+// Function to retrieve a library by its URL-friendly slug from the database
+pub async fn get_library_by_slug(pool: &sqlx::PgPool, slug: String) -> Result<Library, sqlx::Error> {
+    // Execute an SQL query to select a library by its URL-friendly slug
+    let result = sqlx::query_as::<_, Library>(
+        "select * from library where slug = $1"
+    ).bind(slug).fetch_one(pool).await?;
+    // Return the library if found
+    Ok(result)
+}
+
+// Conditional compilation for sqlx when either test or ssr feature is enabled
+#[cfg(any(test, feature = "ssr"))]
+// Function to delete a library from the database
+pub async fn delete_library(pool: &sqlx::PgPool, library: Library) -> Result<u64, sqlx::Error> {
+    // Execute an SQL query to delete a library by its unique identifier
+    let result = sqlx::query("delete from library where id = $1").bind(library.id).execute(pool).await?;
+    // Return the number of rows affected by the delete operation
+    Ok(result.rows_affected())
+}
+
+// Conditional compilation for sqlx when either test or ssr feature is enabled
+#[cfg(any(test, feature = "ssr"))]
+// Function to update a library in the database
+pub async fn update_library(pool: &sqlx::PgPool, id: Uuid, library: Library) -> Result<Library, sqlx::Error> {
+    // Execute an SQL query to update a library by its unique identifier
+    let result : Library = sqlx::query_as::<_, Library>(
+        r#"
+        UPDATE library
+        SET name = $1, slug = $2, street_address = $3, city = $4, state_or_province = $5, postal_code = $6, point_balance = $7, library_system_id = $8
+        WHERE id = $9
+        RETURNING id, name, slug, street_address, city, state_or_province, postal_code, point_balance, library_system_id
+        "#,
+    )
+        .bind(library.name)
+        .bind(library.slug)
+        .bind(library.street_address)
+        .bind(library.city)
+        .bind(library.state_or_province)
+        .bind(library.postal_code)
+        .bind(library.point_balance)
+        .bind(library.library_system_id)
+        .bind(id)
+        .fetch_one(pool)
+        .await?;
+
+    // Return the updated library
+    Ok(result)
 }
--- src/models/library_system.rs
+++ src/models/library_system.rs
@@ -3,10 +3,10 @@ use crate::models::library::Library;
 use uuid::Uuid;
 
 #[cfg(any(test, feature = "ssr"))]
-use {
-    sqlx::PgPool,
-    sqlx::Error,
-};
+use sqlx::PgPool;
+#[cfg(any(test, feature = "ssr"))]
+use sqlx::Error;
+
 
 #[cfg_attr(any(feature = "ssr", test), derive(sqlx::FromRow))]
 #[derive(Serialize, Deserialize)]
@@ -17,6 +17,7 @@ pub struct LibrarySystem {
     pub main_branch_id: Option<Uuid>,
 }
 
+#[derive(Serialize, Deserialize)]
 pub struct CreateLibrarySystem {
     pub name: String,
     pub slug: String,
@@ -54,6 +55,27 @@ pub async fn delete_library_system(pool: &PgPool, ls: LibrarySystem) -> Result<u
     let result = sqlx::query("delete from library_system where id = $1").bind(ls.id).execute(pool).await?;
     Ok(result.rows_affected())
 }
+
+#[cfg(any(test, feature = "ssr"))]
+pub async fn update_library_system(pool: &PgPool, id: Uuid, ls: LibrarySystem) -> Result<LibrarySystem, Error> {
+    let result : LibrarySystem = sqlx::query_as::<_, LibrarySystem>(
+        r#"
+        UPDATE library_system
+        SET name = $1, slug = $2, main_branch_id = $3
+        WHERE id = $4
+        RETURNING id, name, slug, main_branch_id
+        "#,
+    )
+        .bind(ls.name)
+        .bind(ls.slug)
+        .bind(ls.main_branch_id)
+        .bind(id)
+        .fetch_one(pool)
+        .await?;
+
+    Ok(result)
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -99,7 +121,7 @@ mod tests {
         assert_eq!(ls.id.is_nil(), false);
 
 
-        let _ = delete_library_system(&connection, ls);
+        let _ = delete_library_system(&connection, ls).await;
     }
 
     #[actix_web::test]
@@ -116,7 +138,7 @@ mod tests {
         let library_system = get_library_system_by_id(&connection, ls.id).await.expect("failed to grab library"); 
         assert_eq!(ls.id, library_system.id);
 
-        let _ = delete_library_system(&connection, ls);
+        let _ = delete_library_system(&connection, ls).await;
     }
 
     #[actix_web::test]
@@ -125,18 +147,93 @@ mod tests {
 
         let library_system = CreateLibrarySystem {
             name: String::from("Central Arkansas Library System"),
-            slug: String::from("cals"),
+            slug: String::from("tst1"),
         };
 
         let library_system = create_library_system(&connection, library_system).await.expect("failed to create library system");
-        let retrieved_library_system = get_library_system_by_slug(&connection, String::from("cals")).await.expect("failed to retreive library system");
+        let retrieved_library_system = get_library_system_by_slug(&connection, String::from("tst1")).await.expect("failed to retreive library system");
         assert_eq!(library_system.slug, retrieved_library_system.slug);
 
         let _ = delete_library_system(&connection, retrieved_library_system).await;
     }
 
     #[actix_web::test]
-    async fn test_delete_library_system() {
+    async fn library_system_update_with_valid_data() {
         let connection = get_db_connection().await;
+
+        let library_system = CreateLibrarySystem {
+            name: String::from("Old Library"),
+            slug: String::from("ol"),
+        };
+
+        let ls = create_library_system(&connection, library_system).await.expect("Unable to create library");
+
+        let updated_library_system = LibrarySystem {
+            id: ls.id,
+            name: String::from("Updated Library"),
+            slug: String::from("ul"),
+            main_branch_id: None,
+        };
+
+        let updated_ls = update_library_system(&connection, ls.id, updated_library_system).await.expect("Unable to update library");
+
+        assert_eq!(updated_ls.name, "Updated Library");
+        assert_eq!(updated_ls.slug, "ul");
+
+        let _ = delete_library_system(&connection, updated_ls).await;
+    }
+
+    #[actix_web::test]
+    async fn library_system_update_with_invalid_id() {
+        let connection = get_db_connection().await;
+
+        let updated_library_system = LibrarySystem {
+            id: Uuid::new_v4(),
+            name: String::from("Updated Library"),
+            slug: String::from("ul"),
+            main_branch_id: None,
+        };
+
+        let result = update_library_system(&connection, Uuid::new_v4(), updated_library_system).await;
+
+        assert!(result.is_err());
+
+        let error = result.err().unwrap();
+        assert_eq!(error.to_string(), "no rows returned by a query that expected to return at least one row");
+    }
+
+    #[actix_web::test]
+    async fn library_system_update_with_existing_slug() {
+        let connection = get_db_connection().await;
+
+        let library_system1 = CreateLibrarySystem {
+            name: String::from("Library 1"),
+            slug: String::from("l1"),
+        };
+
+        let library_system2 = CreateLibrarySystem {
+            name: String::from("Library 2"),
+            slug: String::from("l2"),
+        };
+
+        let ls1 = create_library_system(&connection, library_system1).await.expect("Unable to create library 1");
+        let ls2 = create_library_system(&connection, library_system2).await.expect("Unable to create library 2");
+
+        let updated_library_system = LibrarySystem {
+            id: ls2.id,
+            name: String::from("Updated Library"),
+            slug: String::from("l1"),
+            main_branch_id: None,
+        };
+
+        let result = update_library_system(&connection, ls2.id, updated_library_system).await;
+
+        assert!(result.is_err());
+
+        let error = result.err().unwrap();
+        assert_eq!(error.to_string(), "error returned from database: duplicate key value violates unique constraint \"library_system_slug_key\"");
+
+        let _ = delete_library_system(&connection, ls1).await;
+        let _ = delete_library_system(&connection, ls2).await;
     }
 }
